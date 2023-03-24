2cfe88908e718509a0e1fac3d115a9438dc03861
Lufuno Maphwanya <lufunom@Thes-MacBook-Pro.local>
2023-03-25 00:27:29 +0200

    adding forms modules.

--- pet_store/urls.py
+++ pet_store/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.views.generic import TemplateView
+from useraccount.views import registration_view, login_view, logout_view, update_user_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name="guest/home.html"), name='home'),
-    path('/register', TemplateView.as_view(template_name="guest/register.html"), name='register'),
+    path('register', registration_view, name="register"),
+    path('login', login_view, name="login"),
+    path('logout', logout_view, name="logout"),
+    path('profile', update_user_view, name="profile"),
+
 ]
---
+++ useraccount/forms/__init__.py
@@ -0,0 +1,3 @@
+from login_form import LoginForm
+from registration_form import RegistrationForm
+from update_form import UpdateForm
