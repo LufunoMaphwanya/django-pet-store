cfb3c8c8110fbc01399ed561ab68b89495901cfb
Lufuno Maphwanya <lufunom@Thes-MacBook-Pro.local>
2023-03-24 23:50:15 +0200

    Adds bootrap and template skeleton.

--- pet_store/settings.py
+++ pet_store/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'guest',
+    'useraccount',
 ]
 
 MIDDLEWARE = [
@@ -101,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -114,10 +114,13 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#Add this in your settings.py file:
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- pet_store/urls.py
+++ pet_store/urls.py
@@ -20,5 +20,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name="guest/home.html")),
+    path('', TemplateView.as_view(template_name="guest/home.html"), name='home'),
+    path('/register', TemplateView.as_view(template_name="guest/register.html"), name='register'),
 ]
